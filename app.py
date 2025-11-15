d14adc5114c538ae6d984d0bbe132e2f5085ad69
amaturetexter <meghapunneshetty@gmail.com>
2025-11-15 13:47:35 +0530

    code pushed

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, send_file
 from transformers import pipeline
 
 app = Flask(__name__)
 generator = pipeline('text-generation', model='gpt2')
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    generated_text = ''
-    if request.method == 'POST':
-        prompt = request.form.get('prompt')
+@app.route('/', methods=['GET'])
+def home():
+    return send_file('index.html')
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    prompt = request.form.get('prompt')
+    if prompt:
         result = generator(prompt, max_length=100)
         generated_text = result[0]['generated_text']
-    return render_template('index.html', generated_text=generated_text)
+        return generated_text
+    return ''
 
 if __name__ == '__main__':
     app.run(debug=True)
